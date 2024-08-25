359c01a4223df65cb6da002c033f0a50489dde4e
Ashish Rai <ashish@aiztechlabs.com>
2024-08-25 12:17:17 -0400

    comment UI

--- simplicity-react/src/pages/PostPage.jsx
+++ simplicity-react/src/pages/PostPage.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getPostById } from "../api/api";
+import { createComment, getCommentsByPostId, getPostById } from "../api/api";
 import "./PostPage.css";
+import moment from "moment";
 
 const HtmlContent = ({ content }) => {
   return <div dangerouslySetInnerHTML={{ __html: content }} />;
@@ -12,6 +13,19 @@ const SinglePostPage = () => {
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+
+  const fetchComments = async () => {
+    const comments = await getCommentsByPostId(postId);
+    setComments(comments.data);
+    console.log("comments::", comments.data);
+  };
+
+  const addComment = async () => {
+    await createComment({ postId, comment });
+    await fetchComments();
+  };
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -28,6 +42,11 @@ const SinglePostPage = () => {
     fetchPost();
   }, [postId]);
 
+  useEffect(() => {
+    fetchComments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
@@ -37,7 +56,26 @@ const SinglePostPage = () => {
         <div>
           <h1>{post.title}</h1>
           <HtmlContent content={post.content} />
-          {/* <p>{post.content}</p> */}
+
+          {comments.map((cmt) => {
+            return (
+              <div className="comment-card" key={cmt?.idß}>
+                <div className="avatar-wrapper">J</div>
+                <div className="comment-content-wrapper">
+                  <div className="comment-header">
+                    Jack: at <span className="comment-ago">{moment(cmt.commented_at).fromNow()}</span>
+                  </div>
+                  <div className="comment-content">{cmt.comment}</div>
+                </div>
+              </div>
+            );
+          })}
+          <div className="add-comment-wrapper">
+            <textarea className="comment-textarea" onBlur={(e) => setComment(e.target.value)}></textarea>
+          </div>
+          <button className="add-comment-button" onClick={addComment}>
+            Add comment
+          </button>
         </div>
       ) : (
         <p>Post not found</p>
