c753baca55b04176420e032cbf055be4d0ccf6c5
Ashish Rai <ashish@aiztechlabs.com>
2024-08-25 13:51:57 -0400

    env

--- simplicity-react/src/api/api.js
+++ simplicity-react/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; // Replace with your Node.js API URL
+const API_URL = process.env.API_URL || 'http://localhost:5000/api'; // Replace with your Node.js API URL
 
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
